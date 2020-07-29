a0a0ce6737249c58f1e2473dbf17d165521758a3
Sabri Abbassi <sabri.abbassidoukkali@gmail.com>
2020-07-30 01:50:04 +0200

    Controller Show +templates +routes

---
+++ src/Controller/ShowController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ShowRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ShowController extends AbstractController
+{
+    /**
+     * @Route("/show", name="show") 
+     */
+    public function index(ShowRepository $repo)
+    {   
+        $shows = $repo->findAll();
+
+        return $this->render('show/index.html.twig', [
+            'shows' => $shows,
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="show_show")
+     */
+    public function show($id, ShowRepository $repo)
+    {   
+        $show = $repo->find($id);
+
+        return $this->render('show/show.html.twig', [
+            'show' => $show,
+        ]);
+    }
+}
